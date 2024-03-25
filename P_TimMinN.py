d9efb94a13e7934b041e8b16892d7b69d20e06c6
TamNhien <canhthien91@gmail.com>
2024-03-25 21:13:50 +0700

    Add project files.

---
+++ P_TimMinN.py
@@ -0,0 +1,8 @@
+﻿A = float(input("Nhập vào giá trị A : "))
+
+n, sum_terms = 1, 0
+while (sum_terms := sum_terms + 1/n) <= A:
+    n += 1
+
+print("Giá trị của n nhỏ nhất sao cho tổng 1 + 1/2 + 1/3 + ... + 1/n lớn hơn", A, "là : ", n - 1)
+
